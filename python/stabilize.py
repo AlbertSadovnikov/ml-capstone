c7f34f020a10181019406028934fea8eea1e33fd
Albert Sadovnikov <albert.sadovnikov@gmail.com>
2017-05-31 23:58:22 +0300

    Working stabilizer code

--- python/stabilize.py
+++ python/stabilize.py
@@ -2,7 +2,6 @@ import cv2
 import numpy as np
 from argparse import ArgumentParser
 
-
 parser = ArgumentParser(description='Drone traffic video stabilization')
 parser.add_argument('--video', help='Path to video file', dest='video_filename', required=True)
 parser.add_argument('--output', help='Path to output video file', dest='out_filename', required=True)
@@ -22,53 +21,67 @@ print(' %d frames %d x %d @ %.2f fps' % (frameCount, frameWidth, frameHeight, fr
 print('-' * 50)
 
 # parameters for Shi Tomasi corner detection
-feature_params = dict(maxCorners=4096,
-                      qualityLevel=0.1,
-                      minDistance=16,
-                      blockSize=16)
+feature_params = dict(maxCorners=256,
+                      qualityLevel=0.25,
+                      minDistance=32,
+                      blockSize=32)
 
 # Parameters for Lucas Kanade optical flow
-lk_params = dict(winSize=(16, 16),
+lk_params = dict(winSize=(32, 32),
                  maxLevel=2,
                  criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
+# Find homography parameters
+hg_params = dict(method=cv2.RANSAC,
+                 ransacReprojThreshold=0.5)
+
 # Create some random colors
 color = np.random.randint(0, 255, (feature_params['maxCorners'], 3))
+
+cv2.namedWindow('Stabilized', cv2.WINDOW_NORMAL)
+
 # Take first frame and find corners in it
-ret, old_frame = cap.read()
+ret, base_frame = cap.read()
+# cv2.imshow('Stabilized', base_frame)
+base_gray = cv2.cvtColor(base_frame, cv2.COLOR_BGR2GRAY)
+p0 = cv2.goodFeaturesToTrack(base_gray, mask=None, **feature_params)
 
-old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
+currentTransform = np.eye(3)
 
-p0 = cv2.goodFeaturesToTrack(old_gray, mask=None, **feature_params)
-
-print('Got %d points to track' % len(p0))
-# Create a mask image for drawing purposes
-
-mask = np.zeros_like(old_frame)
+counter = 0
 while True:
+    # get good tracking points
+    counter += 1
+    print('Frame %d' % counter)
+
+    # print('\tDetected %d good key points to track' % len(p0))
+    # read next frame
     ret, frame = cap.read()
+    if not ret:
+        break
+    # apply current transformation
+    correctedFrame = cv2.warpPerspective(frame,
+                                         currentTransform,
+                                         (1920, 1080),
+                                         flags=cv2.INTER_LANCZOS4,
+                                         borderMode=cv2.BORDER_CONSTANT,
+                                         borderValue=(0, 0, 255))
+    # convert to gray
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # calculate optical flow
-    p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+    p1, st, err = cv2.calcOpticalFlowPyrLK(base_gray, frame_gray, p0, None, **lk_params)
     # Select good points
-    good_new = p1[st == 1]
-    good_old = p0[st == 1]
-
-    # draw the tracks
-    for i, (new, old) in enumerate(zip(good_new, good_old)):
-        a, b = new.ravel()
-        c, d = old.ravel()
-        mask = cv2.line(mask, (a, b), (c, d), color[i].tolist(), 2)
-        frame = cv2.circle(frame, (a, b), 5, color[i].tolist(), -1)
-    img = cv2.add(frame, mask)
-    cv2.imshow('frame', img)
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
-        break
-
-    # Now update the previous frame and previous points
-    old_gray = frame_gray.copy()
-    p0 = good_new.reshape(-1, 1, 2)
+    good_p1 = p1[st == 1]
+    good_p0 = p0[st == 1]
+    res, inliers = cv2.findHomography(good_p1, good_p0, **hg_params)
+    inlierNum = np.sum(inliers)
+    print('\t%d inliers out of %d(%d)' % (inlierNum, len(good_p0), len(p0)))
+    currentTransform = np.dot(currentTransform, res)
+    cv2.imshow('Stabilized', correctedFrame)
+    cv2.waitKey(1)
+    # copy
+    base_gray = frame_gray.copy()
+    p0 = p1.copy()
 
 cv2.destroyAllWindows()
 cap.release()
