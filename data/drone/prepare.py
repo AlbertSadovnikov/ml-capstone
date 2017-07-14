3f4ad407c78188292960c7a111f0982a29099a34
Albert Sadovnikov <albert.sadovnikov@gmail.com>
2017-07-14 15:32:55 +0300

    Reorganized dataset

---
+++ data/drone/prepare.py
@@ -0,0 +1,32 @@
+import os
+import cv2
+
+video_file = 'test_00.mp4'
+output_file_pattern = 'images/%06d.jpg'
+
+cap = cv2.VideoCapture(video_file)
+
+# NB Only images with step 150 are labeled (range(0, number_of_frames, 150))
+frame_skip = 150
+
+if not os.path.exists('images'):
+    os.makedirs('images')
+
+# get video parameters
+frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+framePS = cap.get(cv2.CAP_PROP_FPS)
+
+
+for idx in range(0, frameCount, frame_skip):
+    cap.set(cv2.CAP_PROP_POS_FRAMES, idx)
+    ret, frame = cap.read()
+    if not ret:
+        break
+    filename = output_file_pattern % idx
+    cv2.imwrite(filename, frame)
+    print('written %s' % filename)
+
+cap.release()
+
